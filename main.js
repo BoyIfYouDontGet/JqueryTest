40cd9ac19bb8e81e9806c2eeb246abc357ce1200
Miguel Garcia <miguelgarcia2515@gmail.com>
2015-04-20 10:41:18 -0700

    test

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $('div').click(function() {
+        
+        alert("Hey you clicked me!");
+    });
+});
+
+$(document).ready(function() {
+    $('div').click(function() {
+        
+        alert("Hey you clicked me again!!");
+    });
+});
+
+
+
